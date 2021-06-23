7066349c7a40f039f99474e1780dee9d2891a736
Tejas1510 <64543913+Tejas1510@users.noreply.github.com>
2021-06-23 19:45:35 +0530

    Add files via upload

---
+++ FTS Quiz App/js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the full form of FTS Organization?",
+    answer: "Failure To Success",
+    options: [
+      "Fast Track Selection",
+      "Foreign Travel Scheme",
+      "Failure To Success",
+      "Flight Test System"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When was FTS founded?",
+    answer: "2020",
+    options: [
+      "2021",
+      "2020",
+      "2019",
+      "2018"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Where is the main headquarter of FTS",
+    answer: "Uttar Pradesh",
+    options: [
+      "Uttar Pradesh",
+      "Maharashtra",
+      "Rajhasthan",
+      "Tamil Nadu"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What are the various internships at FTS",
+    answer: "All of the Above",
+    options: [
+      "Web Development",
+      "Social Media Marketing",
+      "Event Management",
+      "All of the Above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is the current team size of FTS",
+    answer: "11-50",
+    options: [
+      "1-10",
+      "11-50",
+      "50-100",
+      "None of the above"
+    ]
+  },
+];
